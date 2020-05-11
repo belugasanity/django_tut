a874789ad3041b71c2345f1a8d2c681c6512b981
Austin <aust.bagley@gmail.com>
2020-05-11 13:34:12 -0600

    remove urls from root folder, add redirect to polls index

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from polls import views as poll_views
 
 urlpatterns = [
+    path('', poll_views.index, name='index'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
